110254db5d95de63e7c7f9581e94a7f78f572d7c
zhaohaotian <zhaoht@bupt.edu.cn>
2026-02-27 09:29:06 +0800

    chore: round picker margin top 2px

--- frontend/src/components/DemoLibrary/DemoLibrary.vue
+++ frontend/src/components/DemoLibrary/DemoLibrary.vue
@@ -2052,7 +2052,7 @@ const scoreLeftRightMap = computed(() => {
   width: 100%;
   height: 2px;
   background: var(--gh-border);
-  margin-top: 4px;
+  margin-top: 2px;
 }
 
 .demo-bar-round-cell.is-playing .demo-bar-round-underline {
